e044996f812234131d7299c568e9b45c955d4f06
Sunilsahoo96 <sunilsahoo5075@gmail.com>
2025-05-12 03:28:29 +0530

    cors

--- src/socket/socket.ts
+++ src/socket/socket.ts
@@ -1,3 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://backend-k7ta.onrender.com");
+// Use process.env.NODE_ENV to determine if it's development or production
+export const socket = process.env.NODE_ENV === 'production'
+  ? io("https://backend-k7ta.onrender.com") 
+  : io("http://localhost:4000"); // Local development URL
